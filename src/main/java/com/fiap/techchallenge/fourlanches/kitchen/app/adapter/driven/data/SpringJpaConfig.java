a2eec6b7ddb3513ec375358aed0f4a9e1bba211e
Augusto Silva <augusto.a.silva@hotmail.com>
2024-01-28 23:00:26 -0300

    feat: add docker compose, docker and mysql config files

---
+++ src/main/java/com/fiap/techchallenge/fourlanches/kitchen/app/adapter/driven/data/SpringJpaConfig.java
@@ -0,0 +1,9 @@
+package com.fiap.techchallenge.fourlanches.kitchen.app.adapter.driven.data;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.fiap.techchallenge.fourlanches.kitchen.app.adapter.driven.data")
+public class SpringJpaConfig {
+}
